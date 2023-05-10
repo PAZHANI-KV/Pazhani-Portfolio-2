fa9f33dc27793db523754e451cc708aad4359f17
PAZHANI-KV <pazhanivel11111@gmail.com>
2023-05-10 18:23:27 +0530

    c

---
+++ src/Components/Header/RTA.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RTA = () => {
+  return (
+    <div className='rta'>
+        <a href='https://drive.google.com/file/d/1PvXgtmr2Cu29VwWLdCabvaU-M8LyyxN_/view?usp=sharing' className='btn' target='_blank' rel="noreferrer">Get Resume</a>
+        <a href='#contact' className='btn btn-primary'>Let's Connect</a>
+    </div>
+  )
+}
+
+export default RTA
---
+++ src/Components/Projects/ProjectData.js
@@ -0,0 +1,64 @@
+import IMG1 from '../../Asserts/project1.png'
+import IMG2 from '../../Asserts/project2.png'
+import IMG3 from '../../Asserts/project3.png'
+import IMG4 from '../../Asserts/project4.png'
+
+
+export const ProjectData =[
+    {
+      id: 1,
+      image: IMG1,
+      title: 'Web Scrapper application',
+      github: '	https://github.com/gunal15/webScrappingEcommerce-frontEnd',
+      backend:'https://github.com/gunal15/webScrappingEcommerce-backend',
+      demo: '	https://delicate-cascaron-3dc0d1.netlify.app/',
+      made: [
+        'React',
+        'Bootstrap',
+        'NodeJS',
+        'express',
+        'MongoDB'
+      ]
+    }, 
+    {
+      id: 2,
+      image: IMG2,
+      title: 'AWS polymer search app clone',
+      github: 'https://github.com/gunal15/AWS-polymer-search-Frontend',
+      backend:'https://github.com/gunal15/AWS-polymer-search-Backend',
+      demo: 'https://eclectic-haupia-0bab18.netlify.app/search',
+      made:[
+        'React',
+        'NodeJS',
+        'express',
+        'MongoDB'
+      ]
+    },{
+      id: 3,
+      image: IMG3,
+      title: 'Ticket booking app',
+      github: 'https://github.com/gunal15/movie-booking-frontend',
+      backend:'https://github.com/gunal15/movie-ticket-booking-be',
+      demo: 'https://movie-ticket-book.netlify.app/',
+      made:[
+        'React',
+        'NodeJS',
+        'express',
+        'MongoDB'
+      ]
+    },{
+      id: 4,
+      image: IMG4,
+      title: 'Flight booking',
+      github: 'https://github.com/gunal15/flight-fe',
+      backend:'https://github.com/gunal15/flight-booking-backend',
+      demo: 'https://ease-my-trip-app.netlify.app/',
+      made:[
+        'React',
+        'NodeJS',
+        'express',
+        'MongoDB'
+      ]
+    }
+    
+]
