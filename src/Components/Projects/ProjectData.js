89bd8ebd438d3c0af0b2b6fabe525a153913a0a4
PAZHANI-KV <pazhanivel11111@gmail.com>
2023-10-14 10:15:14 +0530

    c

--- src/Components/Projects/ProjectData.js
+++ src/Components/Projects/ProjectData.js
@@ -1,7 +1,5 @@
 import IMG1 from '../../Asserts/project1.png'
 import IMG2 from '../../Asserts/project2.png'
-import IMG3 from '../../Asserts/project3.png'
-import IMG4 from '../../Asserts/project4.png'
 
 
 export const ProjectData =[
@@ -27,32 +25,6 @@ export const ProjectData =[
       github: 'https://github.com/PAZHANI-KV/flight-booking-frontend-master',
       backend:'https://github.com/PAZHANI-KV/flight-booking-backend-master',
       demo: 'https://unique-youtiao-ee2813.netlify.app/',
-      made:[
-        'React',
-        'NodeJS',
-        'express',
-        'MongoDB'
-      ]
-    },{
-      id: 3,
-      image: IMG3,
-      title: 'Note making app',
-      github: 'https://github.com/PAZHANI-KV/noteapp-frontend',
-      backend:'https://github.com/PAZHANI-KV/note-making-app-backend',
-      demo: 'https://comforting-mousse-2e673b.netlify.app/',
-      made:[
-        'React',
-        'NodeJS',
-        'express',
-        'MongoDB'
-      ]
-    },{
-      id: 4,
-      image: IMG4,
-      title: 'AWS Polymer search',
-      github: 'https://github.com/PAZHANI-KV/AWS-polymer-Frontend',
-      backend:'https://github.com/PAZHANI-KV/AWS-Polymer-Backend',
-      demo: 'https://meek-torrone-7eb924.netlify.app/search',
       made:[
         'React',
         'NodeJS',
